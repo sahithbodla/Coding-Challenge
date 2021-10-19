34ce61a60b84d03c2c686a94a145eb2f8895c563
Sahith Bodla <sahith.bodla@gmail.com>
2021-10-20 00:12:38 +0530

    Implemeted Search Functionality

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,30 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
+import './ToDo.css';
 
 const ToDo = ({setId,setToDoId,setTitle}) => {
     const [toDoDataArray,setToDoDataArray] = useState([]);
     const [doSort,setToDoSort] = useState(null);
+    const [search,setSearch] = useState('');
+    const searchRef = useRef();
+
+    let newArray;
+
+    if(!search){
+        newArray = [...toDoDataArray];
+    } else {
+        newArray = toDoDataArray.filter((obj) => {
+            for(let key in obj){
+                if(obj[key].toString().indexOf(search,0) !== -1) {
+                    return true;
+                }
+            }
+            return false;
+        })
+    }
 
     if(doSort !== null){
-        toDoDataArray.sort((a,b) => {
+        newArray.sort((a,b) => {
             if(a.id<b.id){
                 return doSort.direction === "ascending" ? -1 : 1
             } else if(a.id>b.id){
@@ -39,47 +57,52 @@ const ToDo = ({setId,setToDoId,setTitle}) => {
     }
 
     return <React.Fragment>
-        <table>
-            <thead>
+        <div>
+            <div className="containr">
+                <strong>ToDos</strong>
+                <input type="text" placeholder="Search" ref={searchRef} onChange={() => setSearch(searchRef.current.value)}/>
+            </div>       
+            <table>
+                <thead>
 
-                    <tr>
-                        <th onClick={configSort}>
-                            ToDo ID
-                        </th>
-                        <th>
-                            Title
-                        </th>
-                        <th>
-                            Status
-                        </th>
+                        <tr>
+                            <th onClick={configSort}>
+                                ToDo ID
+                            </th>
+                            <th>
+                                Title
+                            </th>
+                            <th>
+                                Status
+                            </th>
 
-                        <th>
-                            Action
-                        </th>
-                    </tr>
-            </thead>
-            <tbody>
-                {toDoDataArray.map((obj) => {
-                    return (
-                        <tr key={obj.id}>
-                            <td>
-                                {obj.id}
-                            </td>
-                            <td>
-                                {obj.title}
-                            </td>
-                            <td>
-                                {obj.completed ? "Complete" : "Incomplete"}
-                            </td>
-                            <td>
-                                <button onClick={() => setData(obj.userId,obj.id,obj.title)}>View User</button>
-                            </td>
+                            <th>
+                                Action
+                            </th>
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>   
-        
+                </thead>
+                <tbody>
+                    {newArray.map((obj) => {
+                        return (
+                            <tr key={obj.id}>
+                                <td>
+                                    {obj.id}
+                                </td>
+                                <td>
+                                    {obj.title}
+                                </td>
+                                <td>
+                                    {obj.completed ? "Complete" : "Incomplete"}
+                                </td>
+                                <td>
+                                    <button onClick={() => setData(obj.userId,obj.id,obj.title)}>View User</button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
+        </div>
     </React.Fragment>
 }
 
